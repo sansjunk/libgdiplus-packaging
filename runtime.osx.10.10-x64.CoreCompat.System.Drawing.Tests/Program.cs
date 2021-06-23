bc8d53262a200d1c915bad6b71b71750fbb6b060
Frederik Carlier <frederik.carlier@quamotion.mobi>
2021-06-23 16:55:39 +0200

    Add simple integration test

---
+++ runtime.osx.10.10-x64.CoreCompat.System.Drawing.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CoreCompat.System.Drawing.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var bitmap = new Bitmap(512, 512))
+            {
+                bitmap.Save("test.bmp", ImageFormat.Bmp);
+                bitmap.Save("test.jpg", ImageFormat.Jpeg);
+                bitmap.Save("test.gif", ImageFormat.Gif);
+                bitmap.Save("test.ico", ImageFormat.Icon);
+                bitmap.Save("test.png", ImageFormat.Png);;
+                bitmap.Save("test.tiff", ImageFormat.Tiff);
+                bitmap.Save("test.wmf", ImageFormat.Wmf);
+            }
+        }
+    }
+}
